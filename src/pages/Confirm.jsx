dcc48215d1986d5dc345eb814b4550f00fe8d3e8
Zeynab <zeynab@MacBook-Air-Zeynab.local>
2023-05-07 18:16:39 +0400

    second page

--- src/pages/Confirm.jsx
+++ src/pages/Confirm.jsx
@@ -1,12 +1,28 @@
-import React from 'react'
-import './Confirm.css'
+import React from "react";
+import "./Confirm.css";
+import confirmImg from "../assets/img/singUp.png";
+import singUpLeftLogo from "../assets/icon/SingInLogo.png";
+
 const Confirm = () => {
   return (
-    <div className='confirm'>
-      <input type="number" />
-      <button>Confirm</button>
+    <div className="confirm">
+      <div className="confirm-container">
+        <div className="confirmLeft">
+          <img className="confirmLeftLogo" src={singUpLeftLogo} alt="#" />
+          <h1 className="confirmLeftTitle">Doğrulama</h1>
+          <p className="confirmLeftSubTitle">
+            Pleace type theverification code sent to
+          </p>
+          <p className="confirmLeftNumber">+994 55 456 78 90</p>
+          <input type="text" name="fincode" placeholder="OTP kodu daxil edin" />
+          <button className="confirmBtn">Davam et</button>
+        </div>
+        <div className="confirmRightImgBox">
+          <img className="confirmRightImg" src={confirmImg} alt="image" />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Confirm
+export default Confirm;
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,42 +1,27 @@
-<<<<<<< HEAD
-import React, { useState } from 'react'
-import './SignUp.css'
-import credentialsHandler from '../helpers/credentialHandler'
-import { signUp } from '../services/authNetwork'
-import { setToken } from '../helpers/tokenSettings'
-=======
-import React from "react";
+import React, { useState } from "react";
 import "./SignUp.css";
 import singUpLeftLogo from "../assets/icon/SingInLogo.png";
 import singUpRightImg from "../assets/img/signInRightImg.png";
->>>>>>> Zaynab
 
 const SignUp = () => {
+  const [credentials, setCredentials] = useState({});
 
-    const [credentials, setCredentials] = useState({})
+  const register = async () => {
+    const token = await signUp(credentials);
+    setToken(token);
+  };
 
-   const register = async ()=> {
-   const token = await signUp(credentials)
-   setToken(token)
-   }
-
-   console.log(credentials)
+  console.log(credentials);
 
   return (
-<<<<<<< HEAD
-    <div className='sign-up'>
-      <input onChange={credentialsHandler(setCredentials)} type="text" name="finCode" placeholder='Fin kod'/>
-      <input onChange={credentialsHandler(setCredentials)} type="number" name="serialNumber" placeholder='Seria Nömrəsi'/>
-      <input onChange={credentialsHandler(setCredentials)} type="number" name="phoneNumber" placeholder='Mobil Nömrə' id="" />
-      <input onChange={credentialsHandler(setCredentials)} type="password" name="password" placeholder='Şifrə' id="" />
-      <button onClick={register}>Sign up</button>
-=======
     <div className="sign-up">
       <div className="sign-up-container">
         <div className="singUpLeft">
           <img className="singUpLeftLogo" src={singUpLeftLogo} alt="#" />
           <h1 className="singUpLeftTitle">Qeydiyyat</h1>
-          <p className="singUpLeftSubTitle">Qeydiyyat prosesinə xoş gelmisiniz!</p>
+          <p className="singUpLeftSubTitle">
+            Qeydiyyat prosesinə xoş gelmisiniz!
+          </p>
           <input type="text" name="fincode" placeholder="Fin kod" />
           <input
             type="number"
@@ -56,7 +41,6 @@ const SignUp = () => {
           <img className="singUpRightImg" src={singUpRightImg} alt="image" />
         </div>
       </div>
->>>>>>> Zaynab
     </div>
   );
 };
