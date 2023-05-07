63fc86262bb8cb5b7e222e1168864575eb574d46
Zeynab <zeynab@MacBook-Air-Zeynab.local>
2023-05-07 16:47:24 +0400

    firt page

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,16 +1,37 @@
-import React from 'react'
-import './SignUp.css'
+import React from "react";
+import "./SignUp.css";
+import singUpLeftLogo from "../assets/icon/SingInLogo.png";
+import singUpRightImg from "../assets/img/signInRightImg.png";
 
 const SignUp = () => {
   return (
-    <div className='sign-up'>
-      <input type="text" name="fincode" placeholder='Fin kod'/>
-      <input type="number" name="serialNumber" placeholder='Seria Nömrəsi'/>
-      <input type="number" name="phoneNumber" placeholder='Mobil Nömrə' id="" />
-      <input type="password" name="password" placeholder='Şifrə' id="" />
-      <button>Sign up</button>
+    <div className="sign-up">
+      <div className="sign-up-container">
+        <div className="singUpLeft">
+          <img className="singUpLeftLogo" src={singUpLeftLogo} alt="#" />
+          <h1 className="singUpLeftTitle">Qeydiyyat</h1>
+          <p className="singUpLeftSubTitle">Qeydiyyat prosesinə xoş gelmisiniz!</p>
+          <input type="text" name="fincode" placeholder="Fin kod" />
+          <input
+            type="number"
+            name="serialNumber"
+            placeholder="Seria Nömrəsi"
+          />
+          <input
+            type="number"
+            name="phoneNumber"
+            placeholder="Mobil Nömrə"
+            id=""
+          />
+          <input type="password" name="password" placeholder="Şifrə" id="" />
+          <button className="sigInBtn">Davam et</button>
+        </div>
+        <div className="singUpRightImgBox">
+          <img className="singUpRightImg" src={singUpRightImg} alt="image" />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default SignUp
+export default SignUp;
