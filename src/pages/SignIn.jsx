204fa26c167fca575ff17a15faf904cd824b8d3d
Zeynab <zeynab@MacBook-Air-Zeynab.local>
2023-05-07 19:12:11 +0400

    sing in

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,25 +1,48 @@
-import React, { useState } from 'react'
-import './SignIn.css'
-import credentialsHandler from '../helpers/credentialHandler'
-import { signIn } from '../services/authNetwork'
+import React, { useState } from "react";
+import "./SignIn.css";
+import credentialsHandler from "../helpers/credentialHandler";
+import singInLeftLogo from "../assets/icon/SingInLogo.png";
+import singInRightImg from "../assets/img/signInRightImg.png";
 
-
+import { signIn } from "../services/authNetwork";
 
 const SignIn = () => {
+  const [credentials, setCredentials] = useState({});
 
-    const [credentials, setCredentials] = useState({}) 
-
-    const login = ()=>{
-        signIn(credentials)
-    }
+  const login = () => {
+    signIn(credentials);
+  };
 
   return (
-    <div className='sign-in'>
-      <input onChange={credentialsHandler(setCredentials)} type="tel" name="phoneNumber" id="" />
-      <input onChange={credentialsHandler(setCredentials)} type="password" name="password" id="" />
-      <button onClick={login}>Daxil Ol</button>
+    <div className="signIn">
+      <div className="signIn-container">
+        <div className="signInLeft">
+          <img className="signInLeftLogo" src={singInLeftLogo} alt="#" />
+          <h1 className="signInLeftTitle">Daxil ol</h1>
+          <input
+            onChange={credentialsHandler(setCredentials)}
+            type="tel"
+            name="phoneNumber"
+            id=""
+            placeholder="E-mail"
+          />
+          <input
+            onChange={credentialsHandler(setCredentials)}
+            type="password"
+            name="password"
+            id=""
+            placeholder="Şifrə"
+          />
+          <button className="confirmBtn" onClick={login}>
+            Daxil Ol
+          </button>
+        </div>
+        <div className="signInRightImgBox">
+          <img className="singUpRightImg" src={singInRightImg} alt="image" />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default SignIn
+export default SignIn;
