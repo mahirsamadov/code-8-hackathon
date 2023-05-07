e1c3e16e840fd891d3b68763a02faea76b1da0df
tahirdibirovsecond <tahirdibirovresearch@gmail.com>
2023-05-07 14:46:28 +0400

    Confirm

---
+++ src/pages/Confirm.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Confirm = () => {
+  return (
+    <div className='confirm'>
+      <input type="number" />
+      <button>Confirm</button>
+    </div>
+  )
+}
+
+export default Confirm
